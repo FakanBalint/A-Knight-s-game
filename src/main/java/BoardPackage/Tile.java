1071c57409f694ed5c3b3fd4f97837bd5cb792f3
FakanBalint <fakan.balint@gmail.com>
2023-05-10 12:27:30 +0200

    Added row col and isOccupied variables, added a constructor for Tile class, Added getter and setter for row col and isOccupied variables

--- src/main/java/BoardPackage/Tile.java
+++ src/main/java/BoardPackage/Tile.java
@@ -1,4 +1,28 @@
 package BoardPackage;
 
 public class Tile {
+
+    private final int row;
+    private final int col;
+    private boolean isOccupied;
+
+    public Tile(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.isOccupied = false;
+
+    }
+
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+    private void setOccupied(boolean isOccupied){
+        this.isOccupied = isOccupied;
+    }
 }
