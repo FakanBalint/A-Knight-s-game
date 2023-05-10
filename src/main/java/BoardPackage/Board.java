79e76b673089efeb12b53f4cd40750b1d39db5a9
FakanBalint <fakan.balint@gmail.com>
2023-05-10 13:01:56 +0200

    Added Methods to the Knight Class to move the Unit the Move method it gets it's Available moveset from the getAvailableMoves method that uses the addIfValidMove method. The addIfValidMove needs row and col and a board where we check if the board's tile is occupied or not if it is not occupied we add it to the available moveset (moves ArrayList) we check for all 8 possible moves for the knight and we return with the ArrayList of Tiles That are Possible moves.Furthermore in the board class added 2 players (2 Knights) wrote a method to initialize them to the board with the place knight method wrote a getter to get the players and wrote a method to visualize the board on the console with the printBoardMethod

--- src/main/java/BoardPackage/Board.java
+++ src/main/java/BoardPackage/Board.java
@@ -5,6 +5,10 @@ public class Board {
 
     private Tile[][] board;
 
+    private Knight player1;
+    private Knight player2;
+    private Knight currentPlayer;
+
     public Board() {
         board = new Tile[8][8];
         for (int i = 0; i < 8; i++) {
@@ -12,6 +16,59 @@ public class Board {
                 board[i][j] = new Tile(i, j);
             }
         }
+        player1 = placeKnight(0,0);
+        player2 = placeKnight(7,7);
+    }
+
+    private Knight placeKnight( int row, int col) {
+        Knight knight = new Knight(row,col);
+        Tile tile = board[row][col];
+        tile.placeUnitOnThisTile(knight);
+        knight.setPosition(tile);
+        return knight;
+    }
+
+    public Tile getTile(int row, int col) {
+        return board[row][col];
+    }
+
+
+    public void printBoard() {
+        System.out.println("0 1 2 3 4 5 6 7");
+        for (int i = 0; i < 8; i++) {
+
+            for (int j = 0; j < 8; j++) {
+                Tile currentTile = board[i][j];
+                if (currentTile.isOccupied()&&currentTile.getPlayer()==null) {
+                    System.out.print("X");
+                    continue;
+                }
+                if (currentTile.getPlayer()==player1) {
+                    System.out.print("P1");
+                    continue;
+                }
+                if (currentTile.getPlayer()==player2){
+                    System.out.print("P2");
+                    continue;
+                }
+
+                else {
+                    System.out.print("- ");
+                }
+            }
+            System.out.println("\t");
+        }
+
+    }
+
+
+
+    public Knight getPlayer1() {
+        return player1;
+    }
+
+    public Knight getPlayer2() {
+        return player2;
     }
 
 }
--- src/main/java/Units/Knight.java
+++ src/main/java/Units/Knight.java
@@ -1,11 +1,58 @@
 package Units;
 import BoardPackage.*;
+
+import java.util.ArrayList;
+
 public class Knight {
 
     private Tile position;
 
     public Knight(int row, int col) {
         position = new Tile(row, col);
+    }
+
+    public Tile getPosition() {
+        return position;
+    }
+
+    public void setPosition(Tile newPosition) {
+        position = newPosition;
+        position.placeUnitOnThisTile(this);
+    }
+
+    public ArrayList<Tile> getAvailableMoves(Board board) {
+        ArrayList<Tile> moves = new ArrayList<Tile>();
+        int row = position.getRow();
+        int col = position.getCol();
+        addIfValidMove(row - 2, col - 1, moves, board);
+        addIfValidMove(row - 2, col + 1, moves, board);
+        addIfValidMove(row - 1, col - 2, moves, board);
+        addIfValidMove(row - 1, col + 2, moves, board);
+        addIfValidMove(row + 1, col - 2, moves, board);
+        addIfValidMove(row + 1, col + 2, moves, board);
+        addIfValidMove(row + 2, col - 1, moves, board);
+        addIfValidMove(row + 2, col + 1, moves, board);
+        return moves;
+    }
+
+    private void addIfValidMove(int row, int col, ArrayList<Tile> moves, Board board) {
+        if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+            Tile tile = board.getTile(row, col);
+            if (!tile.isOccupied()) {
+                moves.add(tile);
+            }
+        }
+    }
+
+    public boolean move(Tile destinationTile, Board board) {
+        ArrayList<Tile> availableMoves = getAvailableMoves(board);
+        if (availableMoves.contains(destinationTile)) {
+            position.removePlayerFromTile();
+            position = destinationTile;
+            position.placeUnitOnThisTile(this);
+            return true;
+        }
+        return false;
     }
 
 }
