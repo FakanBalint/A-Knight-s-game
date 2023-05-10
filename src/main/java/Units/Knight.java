11234f559a929cf3da5b0c2f39e9689cb8ac583a
FakanBalint <fakan.balint@gmail.com>
2023-05-10 12:39:21 +0200

    Made the Knight Unit have position of A tile and made a constructor for the Knight Class

--- src/main/java/Units/Knight.java
+++ src/main/java/Units/Knight.java
@@ -2,5 +2,10 @@ package Units;
 import BoardPackage.*;
 public class Knight {
 
+    private Tile position;
+
+    public Knight(int row, int col) {
+        position = new Tile(row, col);
+    }
 
 }
