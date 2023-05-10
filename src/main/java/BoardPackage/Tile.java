75a6589dd752ddab181f19c14b0bed5022f614e4
FakanBalint <fakan.balint@gmail.com>
2023-05-10 12:42:49 +0200

    Added Player to the Tile Class . The Player is a Knight type class. Added Methods to work with the added class

--- src/main/java/BoardPackage/Tile.java
+++ src/main/java/BoardPackage/Tile.java
@@ -6,6 +6,8 @@ public class Tile {
     private final int col;
     private boolean isOccupied;
 
+    private Knight player;
+
     public Tile(int row, int col) {
         this.row = row;
         this.col = col;
@@ -24,5 +26,27 @@ public class Tile {
     }
     private void setOccupied(boolean isOccupied){
         this.isOccupied = isOccupied;
+    }
+
+    public Knight getPlayer(){
+        if(player == null){
+            return null;
+        }
+        else{
+            return player;
+        }
+    }
+
+    public void removePlayerFromTile(){
+        player = null;
+    }
+
+    private void setPlayer(Knight player){
+        this.player = player;
+    }
+
+    public void placeUnitOnThisTile(Knight player) {
+        setPlayer(player);
+        setOccupied(true);
     }
 }
